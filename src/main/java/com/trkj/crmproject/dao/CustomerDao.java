ed29243885efa4496c7a003b09dbaeae84ccc87c
fairyCDSN <2455456500@qq.com>
2022-05-19 18:00:02 +0800

    5.19-修改覆盖后的数据

--- src/main/java/com/trkj/crmproject/dao/CustomerDao.java
+++ src/main/java/com/trkj/crmproject/dao/CustomerDao.java
@@ -19,10 +19,10 @@ public interface CustomerDao {
                                        @Param("create_time1") String create_time1,@Param("create_time2")String create_time2,
                                        @Param("salesperson_id")int salesperson_id);
     public List<customer> findCustomer1(@Param("customer_name")String customer_name,@Param("customer_stage") String customer_stage,
-                                       @Param("create_time1") String create_time1,@Param("create_time2")String create_time2);
+                                        @Param("create_time1") String create_time1,@Param("create_time2")String create_time2);
     public List<customer> findCustomer2(@Param("customer_name")String customer_name,@Param("customer_stage") String customer_stage,
-                                       @Param("create_time1") String create_time1,@Param("create_time2")String create_time2,
-                                       @Param("salesperson_id")int salesperson_id);
+                                        @Param("create_time1") String create_time1,@Param("create_time2")String create_time2,
+                                        @Param("salesperson_id")int salesperson_id);
     public CusXqVo findCusXq(int customer_id);
     public int findCustomerById(AddVo addVo);
     public int addCustomer(AddVo addVo);
